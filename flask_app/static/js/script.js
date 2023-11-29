cefb0d7fbe757b4b7862a377137f35301d5bba73
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2023-11-29 16:07:30 -0600

    main title and main bg updated

---
+++ flask_app/static/js/script.js
@@ -0,0 +1,9 @@
+const mainTitle = document.querySelector('.main_title');
+
+    function TitleColorAnim(){
+        mainTitle.classList.add("main_title_anim");
+    }
+
+    function removeTitleColorAnim(){
+        mainTitle.classList.remove("main_title_anim");
+    }
