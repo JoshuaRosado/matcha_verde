afbef693295710aa5d4ebc0421d5d5f41e55ca0b
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2023-12-30 18:29:38 -0600

    home page updated

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -70,12 +70,12 @@ current = 0
 
 // =============== AUTO SLIDE EVERY 5s =======================
 
-    // setInterval(function() {
-    //     if (current === slider.length - 1){
-    //         current = -1
-    //     }
-    //     slideNext()
-    // }, 6000);
+    setInterval(function() {
+        if (current === slider.length - 1){
+            current = -1
+        }
+        slideNext()
+    }, 6000);
 // ==========================================================
 
 
