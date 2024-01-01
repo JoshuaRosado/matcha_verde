5009cc3e2efa36a778e2f032f0649418f3b80ead
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2023-12-31 23:36:45 -0600

    base nav and footer created for all templates

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask_app import app
-# from flask_app.controllers import matchas, users
+from flask_app.controllers import users
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port = 5001)
