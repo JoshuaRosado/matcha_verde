6416a67217ec8e317a476223112b3c55388b93cc
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2024-01-09 19:59:57 -0600

    counter in progress, item container, details tags with drop down, star rating function with js and review section added

--- flask_app/static/js/item.js
+++ flask_app/static/js/item.js
@@ -1,6 +1,7 @@
 // ======================== COUNTER =========================
 
 
+
 var decrease = document.getElementById("minus");
 var increase = document.getElementById("plus");
 const countNum = document.getElementById("count");
@@ -18,3 +19,20 @@ function decreaseBtn(){
     label.textContent = count;
 
 }
+// =============================================================
+// ======================== STAR RATING =========================
+
+const stars = document.querySelectorAll(".stars i");
+
+
+stars.forEach((star, index1) => {
+    // event added when star is clicked
+    star.addEventListener("click", () => {
+        stars.forEach((star, index2) => {
+            // add the class to the star clicked and any stars lower index
+            // remove class from any stars higher index
+            index1 >= index2 ? star.classList.add("active") : star.classList.remove("active")
+        });
+    });
+});
+// =============================================================
