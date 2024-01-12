c1d8d22417b38164bc15b80a3e5cb1672fac8454
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2024-01-12 17:10:28 -0600

    item img selection function added

--- flask_app/static/js/item.js
+++ flask_app/static/js/item.js
@@ -1,3 +1,27 @@
+// ======================== IMG SELECTION FUNCTION =========================
+let mainImg = document.getElementById("main_img");
+let smallImg = document.querySelectorAll(".small_img");
+
+smallImg[0].onclick = function(){
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    mainImg.src = smallImg[3].src;
+}
+smallImg[4].onclick = function(){
+    mainImg.src = smallImg[4].src;
+}
+
+
+
+
+// =======================================================================
 // ======================== COUNTER =========================
 
 
