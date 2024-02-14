876755852a62abb1707ace4d0d6ec34329c5f28b
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2024-02-14 16:43:01 -0600

    laptop view nav bug fixed, recipes page in process

--- flask_app/controllers/matchas.py
+++ flask_app/controllers/matchas.py
@@ -7,7 +7,7 @@ from flask import flash
 # ======================= HOME PAGE ========================
 
 @app.route('/home')
-def home():
+def home_page():
     if 'user_id' not in session:
         flash("You must be logged in to access the dashboard.")
         return redirect('/')
@@ -16,21 +16,26 @@ def home():
     return render_template('home.html', user = user, matchas=matchas)
         
 @app.route('/about')
-def about():
+def about_page():
     user = User.get_by_id(session["user_id"])
     return render_template('about.html', user = user)
 
 @app.route('/faq')
-def faq():
+def faq_page():
     user = User.get_by_id(session["user_id"])
     return render_template('faq.html', user = user)
 
 @app.route('/item')
-def item():
+def item_page():
     user = User.get_by_id(session["user_id"])
     return render_template('item.html', user=user)
 
 @app.route('/matchas')
-def matchas():
+def matchas_page():
     user = User.get_by_id(session["user_id"])
     return render_template('matcha.html', user=user)
+
+@app.route('/recipes')
+def recipes_page():
+    user = User.get_by_id(session["user_id"])
+    return render_template('recipes.html', user=user)
