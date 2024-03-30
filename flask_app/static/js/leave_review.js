7294849532ea159ee4162ff8f03d8d663ab91d89
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2024-03-30 16:25:52 -0500

    account dropdown exit btn position fixed, leave review page star rating function added

---
+++ flask_app/static/js/leave_review.js
@@ -0,0 +1,14 @@
+document.querySelector('.rating-wrapper').addEventListener('click', updateStarRating, false);
+function updateStarRating(evt){
+    for(i=0; i < parseInt(evt.target.id); i++){
+        const stars = document.querySelectorAll('.star');
+        for(s=0; s < 5; s++){
+            stars[s].classList.remove('rating-checked');
+        }
+        for(j=0; j < parseInt(evt.target.id); j++){
+            stars[j].classList.add('rating-checked');
+        }
+    }
+    document.querySelector('#rating').value = evt.target.id;
+    console.log("CLICKKKKKKKed")
+}
