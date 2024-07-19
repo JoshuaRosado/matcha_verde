caa3945355f977cd3cb438155d9df03287554592
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2024-07-19 15:33:38 -0500

    star review function bug fixed

--- flask_app/models/review.py
+++ flask_app/models/review.py
@@ -159,32 +159,36 @@ class Review:
         
         review_data = connectToMySQL(DB).query_db(query,data)
         review_matchas = []
+        print(f"*****%%%%%% {data}$$$$")
         for review in review_data:
-            
-            review_obj_matcha = cls(review)
-            
-            review_obj_matcha.matcha = matcha.Matcha(
-            {
-                "id": review["id"],
-                "matcha_name": review["matcha_name"],
-                "matcha_qty": review["matcha_qty"],
-                "matcha_short_description": review["matcha_short_description"],
-                "taste_description": review["taste_description"],
-                "taste_notes": review["taste_notes"],
-                "price": review["price"],
-                "img": review["img"],
-                "created_at": review["created_at"],
-                "updated_at": review["updated_at"],
-                "small_img_one": review["small_img_one"],
-                "small_img_two": review["small_img_two"],
-                "small_img_three": review["small_img_three"],
-                "small_img_four": review["small_img_four"]
                 
-            }
-        )
-            review_matchas.append(review_obj_matcha)
-            print(f"^^^^^^{review_obj_matcha}")
-        return review_obj_matcha
+                print(f"####{review}")
+                
+                review_obj_matcha = cls(review)
+                
+                review_obj_matcha.matcha = matcha.Matcha(
+                {
+                    "id": review["id"],
+                    "matcha_name": review["matcha_name"],
+                    "matcha_qty": review["matcha_qty"],
+                    "item_qty":review ["item_qty"],
+                    "matcha_short_description": review["matcha_short_description"],
+                    "taste_description": review["taste_description"],
+                    "taste_notes": review["taste_notes"],
+                    "price": review["price"],
+                    "img": review["img"],
+                    "created_at": review["created_at"],
+                    "updated_at": review["updated_at"],
+                    "small_img_one": review["small_img_one"],
+                    "small_img_two": review["small_img_two"],
+                    "small_img_three": review["small_img_three"],
+                    "small_img_four": review["small_img_four"]
+                    
+                }
+            )
+                review_matchas.append(review_obj_matcha)
+                print(f"^^^^^^{review_obj_matcha}")
+                return review_obj_matcha
 
     # =================== VALIDATE REVIEW'S INPUT ==========================
     @staticmethod
--- flask_app/static/erd/matcha_verde.sql
+++ flask_app/static/erd/matcha_verde.sql
@@ -1,13 +1,20 @@
+-- Disable the foreign key to be able to insert and update rows
+-- SET GLOBAL FOREIGN_KEY_CHECKS=1;
+-- SHOW GLOBAL VARIABLES LIKE 'FOREIGN_KEY_CHECKS'; 
+
+-- select * from matchas
+
+
 
 INSERT INTO matchas( id, matcha_name, matcha_qty,item_qty, 
 matcha_short_description, taste_description,taste_notes, price, img, 
 small_img_one,small_img_two, small_img_three, small_img_four,
 created_at, updated_at, user_id, bag_id)
 
-VALUES(1, "Ummon", "40g", 1, "Rich & Robust", 
-"Ummon has a powerful umami, deep emerald green color, and sweet aroma. This matcha is a treat to be savored.",
-"For decades Ummon has been one of the richest, most full-forced matcha blends in our selection. Our blending and tasting team members have shared their personal tasting comments on this tea below.",
-30, "matcha_verde_website/flask_app/static/imgs/matcha_types_imgs/ippodo-tea-ummon-40g-matcha_98cfdb6b-463f-472e-aced-416ce61f7405_750x750.png",
-"matcha_verde_website/flask_app/static/imgs/item_imgs/ummon_small_imgs/ummon_small1.avif", "matcha_verde_website/flask_app/static/imgs/item_imgs/ummon_small_imgs/ummon_small2.webp",
-"matcha_verde_website/flask_app/static/imgs/item_imgs/ummon_small_imgs/ummon_small3.webp", NULL, 
-NOW(), NOW(),1,1)
+VALUES(3, "Sayaka", "100g", 1, "Rich & Smooth", "Our recommendation for people new to our matcha, Sayaka is a more light-hearted, smooth tea in the rich category. Its sweetness and umami are accompanied by a hint of bitterness.
+",
+"We recommend Sayaka for your first taste of Ippodo matcha, as it is a smooth blend with umami and just a hint of astringency. 
+Try this matcha as koicha to experience the full depth and nuances of its character. As usucha, Sayaka is an accessible tea to serve to beginners and connoisseurs alike at teatime. For matcha lattes, we usually recommend a medium-to-light category matcha with more astringency, but some will choose Sayaka for its bright color, umami, and soft astringency.",
+75, "item_imgs/sayaka_small_imgs/Sayaka_big_img.webp", "item_imgs/sayaka_small_imgs/Sayaka_big_img.webp",
+"item_imgs/sayaka_small_imgs/sayaka_small2.webp", "item_imgs/sayaka_small_imgs/sayaka_small3.webp", "item_imgs/sayaka_small_imgs/Sayaka100g-plate_1024x1.webp",
+NOW(), NOW(), 1,1)
